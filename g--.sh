609e43209ef112ee5e478bac80f18f88f18333e7
1progwriter1 <112411575+1progwriter1@users.noreply.github.com>
2024-05-09 00:04:47 +0300

    shell script ready

--- g--.sh
+++ g--.sh
@@ -3,17 +3,25 @@
 # Переменная для управления выводом команд в консоль
 VERBOSE=true
 
-# Функция для выполнения команды с возможностью вывода в консоль
+# Функция для выполнения команды с возможностью вывода в консоль и проверкой кода завершения
 execute_command() {
     local command="$1"
     if [ "$VERBOSE" = true ]; then
         echo "Выполняется команда: $command"
     fi
     eval "$command"
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Ошибка: Команда завершилась с кодом $exit_code"
+        exit $exit_code
+    fi
 }
 
 # Путь к исполняемым файлам
 EXECUTABLES_DIR="$(dirname "$0")/my_g--"
+
+# Путь к файлу dump.txt
+DUMP_FILE="dump.txt"
 
 # Проверяем, передан ли аргумент в скрипт
 if [ $# -ne 1 ]; then
@@ -56,6 +64,9 @@ execute_command "mkdir -p '$filename'"
 execute_command "mv '$filename.mo' '$filename/'"
 execute_command "mv 'graphviz/graph.png' '$filename/'"
 
+# Перемещаем файл dump.txt в папку с остальными созданными файлами
+execute_command "mv '$DUMP_FILE' '$filename/'"
+
 # Запускаем ./back.out с файлом .mo в качестве аргумента
 execute_command "$EXECUTABLES_DIR/back.out '$filename/$filename.mo'"
 
@@ -65,10 +76,16 @@ if [ ! -e "$filename/$filename.ms" ]; then
     exit 1
 fi
 
-# Перемещаем файл .ms в главный каталог
-execute_command "mv '$filename/$filename.ms' $filename/"
+# Перемещаем файл .ms в папку с остальными промежуточными файлами
+execute_command "mv '$filename.ms' '$filename/'"
 
 # Запускаем ./asm.out с файлом .ms в качестве аргумента и указываем имя выходного файла .mout
 execute_command "$EXECUTABLES_DIR/asm.out '$filename/$filename.ms' '$filename.mout'"
+
+# Проверяем, был ли создан файл .mout
+if [ ! -e "$filename.mout" ]; then
+    echo "Не удалось создать файл .mout"
+    exit 1
+fi
 
 echo "Процесс завершен"
